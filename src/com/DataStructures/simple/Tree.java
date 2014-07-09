3b0b12ab8b4a460aaf7debe507ff718d0b002d57
Knight Fu <knight.w.fu@gmail.com>
2014-07-08 23:04:50 -0400

    now we have a find node

--- src/com/DataStructures/simple/Tree.java
+++ src/com/DataStructures/simple/Tree.java
@@ -1,5 +1,6 @@
 package com.DataStructures.simple;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * Created with IntelliJ IDEA.
@@ -9,9 +10,31 @@ import java.util.List;
  * To change this template use File | Settings | File Templates.
  */
 public class Tree<E> {
-   private E root;
+   private Node<E> root;
 
    public Tree() {
+      this.root = null;
+   }
+
+   private Node<E> findNode( E target ){
+      Node<E> targetNode = null;
+
+      // do a breadth first search of the tree
+      if(this.root == null)
+         return null;
+
+      Stack<Node<E>> nodeStack = new Stack<Node<E>>();
+      nodeStack.push( this.root );
+      while( !nodeStack.empty() ){
+         Node<E> currentNode = nodeStack.pop();
+         if( currentNode.getData() == target )
+            return currentNode;
+
+         for( Node<E> childNode : currentNode.getChildren())
+            nodeStack.push( childNode );
+      }
+
+      return targetNode;
    }
 
    public Tree<E> getSubtree( Node<E> rootNode ) {
@@ -31,6 +54,14 @@ public class Tree<E> {
       public Node( Node<E> parent, E data ) {
          this.parent = parent;
          this.data = data;
+      }
+
+      public E getData(){
+         return this.data;
+      }
+
+      public List<Node<E>> getChildren(){
+         return this.children;
       }
    }
 }
